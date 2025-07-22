02188fe181e48767b890a2b29a25e3a00974e386
unknown <dangabarin2020@gmail.com>
2025-07-22 11:09:46 +0100

    update commit

--- controllers/sendmail.js
+++ controllers/sendmail.js
@@ -15,78 +15,78 @@ const amqp={
   }
 
  
-// const mails=async(req,res)=>{
-//     const{subject,mailList,ht}=req.body
-//     const list= mailList.split(",")
-//     console.log(list)
-// try {
-//     let config= {
-//         host:"smtp.gmail.com",
-//         port: 465,
-//         secure: true, // use SSL  
-//          auth:{ 
-//                 user:'dangabarin2020@gmail.com',
-//                 pass:"yabccxpsciuoynqs"
-//             },      
-//       } 
-//     const mail_config={
-//         from :'memet omar <dangabarin2020@gmail.com>',
-//         to:mailList,
-//         replyTo:"dangabarin2020@gmail.com",
-//         subject: subject,
-//         text: 'hello world!',
-//         html:ht,  
-//         list: {
-//             // List-Help: <mailto:admin@example.com?subject=help>
-//             help: 'memetoumar@gmail.com?subject=help',
-//             // List-Unsubscribe: <http://example.com> (Comment)
-//             unsubscribe: {
-//                 url: 'http://google.com/search',
-//                 comment: 'Click here to unsubscribe'
-//             },
-//             // List-Subscribe: <mailto:admin@example.com?subject=subscribe>
-//             // List-Subscribe: <http://example.com> (Subscribe)
-//             // subscribe: [
-//             //     'admin@example.com?subject=subscribe',
-//             //     {
-//             //         url: 'http://example.com',
-//             //         comment: 'Subscribe'
-//             //     }
-//             // ],
+const previewMail=async(req,res)=>{
+    const{subject,mailList,ht}=req.body
+    const list= mailList.split(",")
+    console.log(list)
+try {
+    let config= {
+        host:"smtp.gmail.com",
+        port: 465,
+        secure: true, // use SSL  
+         auth:{ 
+                user:'dangabarin2020@gmail.com',
+                pass:"yabccxpsciuoynqs"
+            },      
+      } 
+    const mail_config={
+        from :'memet omar <dangabarin2020@gmail.com>',
+        to:mailList,
+        replyTo:"dangabarin2020@gmail.com",
+        subject: subject,
+        text: 'hello world!',
+        html:ht,  
+        list: {
+            // List-Help: <mailto:admin@example.com?subject=help>
+            help: 'memetoumar@gmail.com?subject=help',
+            // List-Unsubscribe: <http://example.com> (Comment)
+            unsubscribe: {
+                url: 'http://google.com/search',
+                comment: 'Click here to unsubscribe'
+            },
+            // List-Subscribe: <mailto:admin@example.com?subject=subscribe>
+            // List-Subscribe: <http://example.com> (Subscribe)
+            // subscribe: [
+            //     'admin@example.com?subject=subscribe',
+            //     {
+            //         url: 'http://example.com',
+            //         comment: 'Subscribe'
+            //     }
+            // ],
            
           
-//         } 
-//       }
-//       let transport =nodemailer.createTransport(config)
-//      // Send the message using the previously set up Nodemailer transport
-//      transport.sendMail(mail_config, (err, info) => {
-//       if (err) {
-//           console.error(err.stack);
-//           // put the failed message item back to queue
-//            channel.nack(data);
-//       }
+        } 
+      }
+      let transport =nodemailer.createTransport(config)
+     // Send the message using the previously set up Nodemailer transport
+     transport.sendMail(mail_config, (err, info) => {
+      if (err) {
+          console.error(err.stack);
+          // put the failed message item back to queue
+           channel.nack(data);
+      }
      
-//       console.log('Delivered message %s', info);
-//       res.status(200).json({msg:"email sent successfully!"})
-//   }) 
+      console.log('Delivered message %s', info);
+      res.status(200).json({msg:"email sent successfully!"})
+  }) 
     
-// } catch (error) {
-//     res.status(500).json({msg:"something went wrong!"}) 
-// }
+} catch (error) {
+    res.status(500).json({msg:"something went wrong!"}) 
+}
 
 
-// //    await  rabbitProvider(amqp,res,{...req.body,list})
-//         // rabbitconsumer(amqp,res,config,list.length)
+//    await  rabbitProvider(amqp,res,{...req.body,list})
+        // rabbitconsumer(amqp,res,config,list.length)
        
 
      
-//     //    rabbitProvider(config,res).then(()=>{
-//     //     rabbitconsumer(config,res)
-//     //    })
+    //    rabbitProvider(config,res).then(()=>{
+    //     rabbitconsumer(config,res)
+    //    })
      
    
        
-//    }
+   }
    const schedule=async(req,res)=>{
     const{subject,mailList,ht,schedule,sendTime}=req.body
     // const contact= mailList.split(",")
@@ -207,4 +207,4 @@ res.status(500).send('server error ')
        
    }
 
-   module.exports={mail,schedule}
+   module.exports={mail,schedule,previewMail}
--- routes/mail.js
+++ routes/mail.js
@@ -1,9 +1,10 @@
 const express= require("express");
 const mailRoute=express.Router();
-const {mail,schedule} =require("../controllers/sendmail")
+const {mail,schedule,previewMail} =require("../controllers/sendmail")
 
 // send mail to queue
 
+mailRoute.post("/preview",previewMail);
 mailRoute.post("/",mail);
 mailRoute.post("/schedule",schedule);
 
